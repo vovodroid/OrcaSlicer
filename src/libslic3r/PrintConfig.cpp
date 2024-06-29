5ddb8b582a1b2b97f329055d60409bf6b48c76e0
vovodroid <vovodroid@users.noreply.github.com>
2024-06-29 12:14:47 +0300

    Infill angles -180/180

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -942,7 +942,8 @@ void PrintConfigDef::init_fff_params()
         "automatically. Otherwise the provided angle will be used for external bridges. "
         "Use 180°for zero angle.");
     def->sidetext = L("°");
-    def->min = 0;
+    def->min = -180;
+    def->max =  180;
     def->mode = comAdvanced;
     def->set_default_value(new ConfigOptionFloat(0.));
 
@@ -2217,8 +2218,8 @@ void PrintConfigDef::init_fff_params()
     def->category = L("Strength");
     def->tooltip = L("Angle for sparse infill pattern, which controls the start or main direction of line");
     def->sidetext = L("°");
-    def->min = 0;
-    def->max = 360;
+    def->min = -180;
+    def->max =  180;
     def->mode = comAdvanced;
     def->set_default_value(new ConfigOptionFloat(45));
 
@@ -2227,8 +2228,8 @@ void PrintConfigDef::init_fff_params()
     def->category = L("Strength");
     def->tooltip = L("Angle for solid infill pattern, which controls the start or main direction of line");
     def->sidetext = L("°");
-    def->min = 0;
-    def->max = 360;
+    def->min = -180;
+    def->max =  180;
     def->mode = comAdvanced;
     def->set_default_value(new ConfigOptionFloat(45));
 
