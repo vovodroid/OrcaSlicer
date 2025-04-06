eb4d1afd6ebf07c126ff51491692d1a87e3ee9f3
vovodroid <vovodroid@users.noreply.github.com>
2025-04-06 20:08:42 +0300

    Print overhangs after internal

--- src/slic3r/GUI/GUI_Factories.cpp
+++ src/slic3r/GUI/GUI_Factories.cpp
@@ -82,7 +82,7 @@ std::map<std::string, std::vector<SimpleSettingData>>  SettingsFactory::OBJECT_C
                     {"seam_position", "",2},
                     {"slice_closing_radius", "",3}, {"resolution", "",4},
                     {"xy_hole_compensation", "",5}, {"xy_contour_compensation", "",6}, {"elefant_foot_compensation", "",7},
-                    {"make_overhang_printable_angle","", 8},{"make_overhang_printable_hole_size","",9}, {"wall_sequence","",10},
+                    {"make_overhang_printable_angle","", 8},{"make_overhang_printable_hole_size","",9}, {"wall_sequence","",10}, {"print_overhangs_after","",10},
                     {"precise_z_height", "",10}
                     }},
     { L("Support"), {{"brim_type", "",1},{"brim_width", "",2},{"brim_object_gap", "",3},{"brim_flow_ratio", "",4},{"brim_use_efc_outline", "",5},
