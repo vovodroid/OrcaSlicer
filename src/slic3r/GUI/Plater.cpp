c474b8127580414a16fc14970a372376486be955
vovodroid <vovodroid@users.noreply.github.com>
2025-04-25 21:01:21 +0300

    Add stl, step, etc. to recent list

--- src/slic3r/GUI/Plater.cpp
+++ src/slic3r/GUI/Plater.cpp
@@ -11353,6 +11353,7 @@ void Plater::add_file()
                 p->set_project_name(from_u8(full_path.stem().string()));
             }
             wxGetApp().mainframe->update_title();
+            wxGetApp().mainframe->add_to_recent_projects(paths[0].wstring());
         }
         break;
     }
@@ -11374,6 +11375,8 @@ void Plater::add_file()
                 p->set_project_name(from_u8(full_path.stem().string()));
             }
             wxGetApp().mainframe->update_title();
+            for (auto &path : paths)
+                wxGetApp().mainframe->add_to_recent_projects(path.wstring());
         }
         break;
     }
@@ -11391,7 +11394,11 @@ void Plater::add_file()
 
         open_3mf_file(first_file[0]);
         load_files(tmf_file, LoadStrategy::LoadModel);
-        if (!load_files(other_file, LoadStrategy::LoadModel, false).empty()) { wxGetApp().mainframe->update_title();}
+        if (!load_files(other_file, LoadStrategy::LoadModel, false).empty()) {
+            wxGetApp().mainframe->update_title();
+            for (auto &file : other_file)
+                wxGetApp().mainframe->add_to_recent_projects(file.wstring());
+        }
         break;
     default:break;
     }
