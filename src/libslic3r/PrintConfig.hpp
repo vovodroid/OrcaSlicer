7c91459c371f85aa1a38f74fa1b9268c43d74959
Ian Bassi <ian.bassi@outlook.com>
2025-12-30 13:16:47 -0300

    Emit Disable Power Loss Recovery (#11616)
    * Emit Disable Power Loss Recovery
    
    Now only works if it's enabled but the goal it's to force disable it.
    With this change it will always emit the command for BBL or Marlin 2.
    
    Co-Authored-By: Michael Rook <54159303+michaelr0@users.noreply.github.com>
    
    * Refactor power loss recovery G-code comments
    
    * Return empty power loss recovery when no compatible printer
    
    * Update power loss recovery comments
    
    Update label and tooltip for power loss recovery
    
    * Add enum for power loss recovery mode
    
    Refactored power loss recovery configuration to use a new PowerLossRecoveryMode enum instead of a boolean. Updated GCodeWriter and related logic to handle the new enum, allowing for 'printer_configuration', 'enable', and 'disable' options. Updated config handling, legacy value conversion, and default values accordingly.
    
    * Update PrintConfig.cpp
    
    ---------
    
    Co-authored-by: Michael Rook <54159303+michaelr0@users.noreply.github.com>
    Co-authored-by: SoftFever <softfeverever@gmail.com>

--- src/libslic3r/PrintConfig.hpp
+++ src/libslic3r/PrintConfig.hpp
@@ -102,6 +102,13 @@ enum class BedTempFormula {
     count,
 };
 
+// Orca
+enum class PowerLossRecoveryMode {
+    PrinterConfiguration,
+    Enable,
+    Disable,
+};
+
 // BBS
 enum class WallSequence {
     InnerOuter,
@@ -507,6 +514,7 @@ CONFIG_OPTION_ENUM_DECLARE_STATIC_MAPS(PrintHostType)
 CONFIG_OPTION_ENUM_DECLARE_STATIC_MAPS(AuthorizationType)
 CONFIG_OPTION_ENUM_DECLARE_STATIC_MAPS(WipeTowerWallType)
 CONFIG_OPTION_ENUM_DECLARE_STATIC_MAPS(PerimeterGeneratorType)
+CONFIG_OPTION_ENUM_DECLARE_STATIC_MAPS(PowerLossRecoveryMode)
 
 #undef CONFIG_OPTION_ENUM_DECLARE_STATIC_MAPS
 
@@ -1272,7 +1280,7 @@ PRINT_CONFIG_CLASS_DEFINE(
     ((ConfigOptionIntsNullable,        filament_flush_temp))
     // BBS
     ((ConfigOptionBool,                scan_first_layer))
-    ((ConfigOptionBool,                enable_power_loss_recovery))
+    ((ConfigOptionEnum<PowerLossRecoveryMode>, enable_power_loss_recovery))
     ((ConfigOptionBool,                enable_wrapping_detection))
     ((ConfigOptionInt,                 wrapping_detection_layers))
     ((ConfigOptionPoints,              wrapping_exclude_area))
