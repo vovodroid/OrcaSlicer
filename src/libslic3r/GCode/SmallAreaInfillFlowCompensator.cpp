c7b87aa4366e0aed00e90dd4a4111540842f3734
vovodroid <vovodroid@users.noreply.github.com>
2026-04-01 20:01:03 +0300

    Refactor compensator

--- src/libslic3r/GCode/SmallAreaInfillFlowCompensator.cpp
+++ src/libslic3r/GCode/SmallAreaInfillFlowCompensator.cpp
@@ -19,92 +19,60 @@
 
 namespace Slic3r {
 
-bool nearly_equal(double a, double b)
-{
-    return std::nextafter(a, std::numeric_limits<double>::lowest()) <= b && std::nextafter(a, std::numeric_limits<double>::max()) >= b;
-}
+    SmallAreaInfillFlowCompensator::SmallAreaInfillFlowCompensator(const ConfigOptionStrings& config)
+    : Interpolator(config) {
+        try {
+            interpolatorModel = std::make_unique<PchipInterpolatorHelper>(xValues, yValues);
 
-SmallAreaInfillFlowCompensator::SmallAreaInfillFlowCompensator(const Slic3r::GCodeConfig& config)
-{
-    try {
-        for (auto& line : config.small_area_infill_flow_compensation_model.values) {
-            std::istringstream iss(line);
-            std::string        value_str;
-            double             eLength = 0.0;
-
-            if (std::getline(iss, value_str, ',')) {
-                try {
-                    // Trim leading and trailing whitespace
-                    value_str = std::regex_replace(value_str, std::regex("^\\s+|\\s+$"), "");
-                    if (value_str.empty()) {
-                        continue;
+            for (size_t i = 0; i < xValues.size(); i++) {
+                if (i == 0) {
+                    if (!nearly_equal(xValues[i], 0.0)) {
+                        throw Slic3r::InvalidArgument(
+                            "Small Area Flow Compensation: First extrusion length for small area infill compensation model must be 0");
                     }
-                    eLength = std::stod(value_str);
-                    if (std::getline(iss, value_str, ',')) {
-                        eLengths.push_back(eLength);
-                        flowComps.push_back(std::stod(value_str));
+                } else {
+                    if (nearly_equal(xValues[i], 0.0)) {
+                        throw Slic3r::InvalidArgument("Small Area Flow Compensation: Only the first extrusion length for small area infill "
+                                                      "compensation model can be 0");
                     }
-                } catch (...) {
-                    std::stringstream ss;
-                    ss << "Small Area Flow Compensation: Error parsing data point in small area infill compensation model:" << line << std::endl;
-
-                    throw Slic3r::InvalidArgument(ss.str());
+                    if (xValues[i] <= xValues[i - 1]) {
+                        throw Slic3r::InvalidArgument(
+                            "Small Area Flow Compensation: Extrusion lengths for subsequent points must be increasing");
+                    }
                 }
             }
-        }
 
-        for (size_t i = 0; i < eLengths.size(); i++) {
-            if (i == 0) {
-                if (!nearly_equal(eLengths[i], 0.0)) {
-                    throw Slic3r::InvalidArgument("Small Area Flow Compensation: First extrusion length for small area infill compensation model must be 0");
-                }
-            } else {
-                if (nearly_equal(eLengths[i], 0.0)) {
-                    throw Slic3r::InvalidArgument("Small Area Flow Compensation: Only the first extrusion length for small area infill compensation model can be 0");
-                }
-                if (eLengths[i] <= eLengths[i - 1]) {
-                    throw Slic3r::InvalidArgument("Small Area Flow Compensation: Extrusion lengths for subsequent points must be increasing");
+            for (size_t i = 1; i < yValues.size(); ++i) {
+                if (yValues[i] <= yValues[i - 1]) {
+                    throw Slic3r::InvalidArgument(
+                        "Small Area Flow Compensation: Flow compensation factors must strictly increase with extrusion length");
                 }
             }
-        }
 
-        for (size_t i = 1; i < flowComps.size(); ++i) {
-            if (flowComps[i] <= flowComps[i - 1]) {
-                throw Slic3r::InvalidArgument("Small Area Flow Compensation: Flow compensation factors must strictly increase with extrusion length");
+            if (!yValues.empty() && !nearly_equal(yValues.back(), 1.0)) {
+                throw Slic3r::InvalidArgument(
+                    "Small Area Flow Compensation: Final compensation factor for small area infill flow compensation model must be 1.0");
             }
+
+
+        } catch (std::exception& e) {
+            BOOST_LOG_TRIVIAL(error) << "Error parsing small area infill compensation model: " << e.what();
+            throw;
         }
 
-        if (!flowComps.empty() && !nearly_equal(flowComps.back(), 1.0)) {
-            throw Slic3r::InvalidArgument("Small Area Flow Compensation: Final compensation factor for small area infill flow compensation model must be 1.0");
-        }
-
-        flowModel = std::make_unique<PchipInterpolatorHelper>(eLengths, flowComps);
-
-    } catch (std::exception& e) {
-        BOOST_LOG_TRIVIAL(error) << "Error parsing small area infill compensation model: " << e.what();
-        throw;
-    }
-}
-
-SmallAreaInfillFlowCompensator::~SmallAreaInfillFlowCompensator() = default;
-
-double SmallAreaInfillFlowCompensator::flow_comp_model(const double line_length)
-{
-    if(flowModel == nullptr)
-        return 1.0;
-
-    if (line_length == 0 || line_length > max_modified_length()) {
-        return 1.0;
-    }
-
-    return flowModel->interpolate(line_length);
-}
+    };
 
 double SmallAreaInfillFlowCompensator::modify_flow(const double line_length, const double dE, const ExtrusionRole role)
 {
-    if (flowModel &&
-        (role == ExtrusionRole::erSolidInfill || role == ExtrusionRole::erTopSolidInfill || role == ExtrusionRole::erBottomSurface)) {
-        return dE * flow_comp_model(line_length);
+    if (interpolatorModel == nullptr)
+        return dE;
+
+    if (line_length == 0 || line_length > max_modified_x()) {
+        return dE;
+    }
+
+    if ((role == ExtrusionRole::erSolidInfill || role == ExtrusionRole::erTopSolidInfill || role == ExtrusionRole::erBottomSurface)) {
+        return dE * interpolatorModel->interpolate(line_length);
     }
 
     return dE;
